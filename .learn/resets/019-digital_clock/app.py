232c7dbc27b971bcc31bb1baf8f7b7acfbc359fa
Ivan Diaz <ivandla96@gmail.com>
2025-06-04 19:31:45 +0000

    actualizado

--- .learn/resets/019-digital_clock/app.py
+++ .learn/resets/019-digital_clock/app.py
@@ -1,6 +1,6 @@
 # Complete the function to return how many hours and minutes are displayed on the 24h digital clock
 def digital_clock(n):
-  return (n // 60), (n % 60)
+  return None
 
 # Invoke the function with any integer (minutes after midnight)
-print(digital_clock(120))
+print(digital_clock())
--- exercises/021-square_root/app.py
+++ exercises/021-square_root/app.py
@@ -1,1 +1,8 @@
 # Your code here
+import math
+
+def square_root(number):
+    result = round(math.sqrt(number), 2)
+    return result
+
+print(square_root(35))
